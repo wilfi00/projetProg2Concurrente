27a7b3b76fa6dd81f9d0795ebb9613c8684fe7a2
William FLAMBARD <E145853L@U-INF-J-E114-11.iut-nantes.univ-nantes.prive>
2016-12-12 17:50:13 +0100

    Debut Config

---
+++ configuration/ConfigurationFile.java
@@ -0,0 +1,9 @@
+package configuration;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ConfigurationFile {
+    private FileOutputStream fos;
+    private FileInputStream fis;
+}
